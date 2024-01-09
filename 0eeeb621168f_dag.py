71af8d76bdbd653b4f57d9497d6855de981c6290
joana54 <nionciu@gmail.com>
2024-01-09 13:20:28 +0000

    DAG code

---
+++ 0eeeb621168f_dag.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+notebook_task = {
+    'notebook_path': '/Users/nionciu@gmail.com/Pinterest Data Cleaning and Queries',
+}
+
+
+notebook_params = {
+    'Variable': 5
+}
+
+
+
+default_args = {
+    'owner': 'Nicoleta',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('0eeeb621168f_dag',
+    start_date=datetime(2023, 1, 9),
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
